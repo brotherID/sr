468c4e1c2473fd4b7acc92382b8d7d878f3e0001
DESKTOP-L6C90VG\admin <rafiksara1@gmail.com>
2022-05-19 20:39:59 +0100

    add boostrap upload to recruitement-request.html

--- src/main/java/com/poc/sr/config/SecurityConfig.java
+++ src/main/java/com/poc/sr/config/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Configuration
 @EnableWebSecurity
@@ -14,13 +18,33 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
+		/*
+		 * auth.userDetailsService(new UserDetailsService() {
+		 * 
+		 * @Override public UserDetails loadUserByUsername(String username) throws
+		 * UsernameNotFoundException {
+		 * 
+		 * return new User("user", "password", null); }
+		 * 
+		 * });
+		 */
+		  
+
+
+		 
+		
+		
+		
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.headers().frameOptions().disable();
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests().anyRequest().permitAll()
+		/*
+		 * .and() .formLogin()
+		 */;
 	}
 
 	
